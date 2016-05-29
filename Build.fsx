d550240a130586afae1af5581ff3cb64434ace9a
Mark Seemann <mark@ploeh.dk>
2016-05-29 22:41:59 +0200

    Added a build script.

--- Build.fsx
+++ Build.fsx
@@ -1,2 +1,19 @@
-printfn "Doing absolutely nothing..."
-printfn "Succeeded!"
+#r @"packages/FAKE.4.27.0/tools/FakeLib.dll"
+
+open Fake
+
+Target "Clean" <| fun _ ->
+    directExec <| fun info ->
+        info.FileName <- "git"
+        info.Arguments <- "clean -xdf"
+    |> ignore
+
+Target "Build" <| fun _ ->
+    !! "**/BookingApi.sln"
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+
+"Clean"
+==> "Build"
+
+RunTargetOrDefault "Build"
