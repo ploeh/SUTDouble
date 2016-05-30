bafb80e7992a237208ed52d9e6fea9719ddf5c80
Mark Seemann <mark@ploeh.dk>
2016-05-30 22:26:57 +0200

    Added a test step to the build script.

--- Build.fsx
+++ Build.fsx
@@ -1,6 +1,7 @@
 #r @"packages/FAKE.4.27.0/tools/FakeLib.dll"
 
 open Fake
+open Fake.Testing
 
 Target "Clean" <| fun _ ->
     directExec <| fun info ->
@@ -13,7 +14,12 @@ Target "Build" <| fun _ ->
     |> MSBuildRelease "" "Rebuild"
     |> ignore
 
+Target "Test" <| fun _ ->
+    !! "**/bin/Release/*Ploeh.Samples.BookingApi.*Tests*.dll"
+    |> xUnit2 (fun p -> { p with Parallel = ParallelMode.All })
+
 "Clean"
 ==> "Build"
+==> "Test"
 
-RunTargetOrDefault "Build"
+RunTargetOrDefault "Test"
