01585cd03675473a7382ae920f6da84e1f10e6a6
Mark Seemann <mark@ploeh.dk>
2016-05-31 17:44:56 +0200

    Introduced ReservationsController.Post,
    although its implementation is still degenerate.

---
+++ BookingApi.Tests/ReservationsControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Xunit;
+
+namespace Ploeh.Samples.BookingApi.Tests
+{
+    public class ReservationsControllerTests
+    {
+        [Fact]
+        public void PostReturnsCorrectResultWhenDateisInvalid()
+        {
+            var sut = new ReservationsController();
+
+            var actual =
+                sut.Post(
+                    new ReservationDto
+                    {
+                        Date = "Not a date",
+                        Name = "Mark Seemann",
+                        Email = "mark@example.com",
+                        Quantity = 4
+                    });
+
+            var badReq =
+                Assert.IsAssignableFrom<BadRequestErrorMessageResult>(actual);
+            Assert.Equal("Invalid date.", badReq.Message);
+        }
+    }
+}
---
+++ BookingApi/ReservationsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class ReservationsController : ApiController
+    {
+        public IHttpActionResult Post(ReservationDto reservationDto)
+        {
+            return this.BadRequest("Invalid date.");
+        }
+    }
+}
